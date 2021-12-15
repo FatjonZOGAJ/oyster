79555f1ea7f1d72b1091b6390b1c905a0809299c
rafael <rafael.sterzinger@gmx.at>
2021-12-15 17:43:16 +0100

    Change scripts

--- scripts/run_meta_batch.sh
+++ scripts/run_meta_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 2500 --type 4 --meta_batch 2
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 1000 --type 4 --meta_batch 5
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 250 --type 4 --meta_batch 20
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 1200 --type 4 --meta_batch 2
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 600 --type 4 --meta_batch 5
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 150 --type 4 --meta_batch 20
 # 10 is in OOD experiment
