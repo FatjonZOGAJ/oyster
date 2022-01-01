440d0f06230f1a959dfa7533893da7346726ee23
Rafael Sterzinger <rsterzinger@eu-login-02.euler.ethz.ch>
2022-01-01 13:11:50 +0100

    Adjust runtime of experiment

--- scripts/run_ood.sh
+++ scripts/run_ood.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 300 --type 1
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 300 --type 2
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 300 --type 3
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 300 --type 4
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 500 --type 1
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 500 --type 2
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 500 --type 3
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 150 --num_iterations 500 --type 4
--- scripts/run_sparse.sh
+++ scripts/run_sparse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 30  --num_iterations 300 --type 4
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 75  --num_iterations 300 --type 4
-python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 15 --num_iterations 300 --type 4
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 30  --num_iterations 500 --type 4
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 75  --num_iterations 500 --type 4
+python3 launch_experiment.py configs/ant-goal.json --n_train_tasks 15  --num_iterations 500 --type 4
 # 150 is in OOD experiment
